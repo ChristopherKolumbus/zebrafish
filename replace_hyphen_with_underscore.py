53e9669e514e944be2328de2d331e272d7aab278
Tim Hladnik <timhladnik@Tims-MacBook-Air.local>
2017-12-08 11:45:45 +0100

    Simplified the renaming of files

--- replace_hyphen_with_underscore.py
+++ replace_hyphen_with_underscore.py
@@ -1,12 +1,14 @@
 import os
 
+replaceChars = {
+    '-': '_'
+}
+
 input_folder = r'..\..\Documents\zebrafish'
 for filename in os.listdir(input_folder):
     print('Renaming {0}'.format(filename))
-    new_filename = ''
-    for char in filename:
-        if char == '-':
-            new_filename += '_'
-        else:
-            new_filename += char
+    new_filename = filename
+    for search in replaceChars.keys():
+        new_filename = new_filename.replace(search, replaceChars[search])
+
     os.rename(os.path.join(input_folder, filename), os.path.join(input_folder, new_filename))
