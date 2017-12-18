d535301e4fb8bed3b31d463c3c7b758f8a36f59c
thladnik <tim.hladnik@gmail.com>
2017-12-18 20:26:21 +0100

    Evaluation of experiment 2 for poster presentation finished

--- auxfuns.py
+++ auxfuns.py
@@ -1,10 +1,49 @@
 import csv
 from IPython import embed
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 analysis_path = ['..']
+
+
+def adjust_spines(ax, spines = ['left','bottom'], shift_pos = False):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            if shift_pos:
+                spine.set_position(('outward', 10))  # outward by 10 points
+            # spine.set_smart_bounds(True)
+        else:
+            spine.set_color('none')  # don't draw spine
+
+    # turn off ticks where there is no spine
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    elif 'right' in spines:
+        ax.yaxis.set_ticks_position('right')
+    else:
+        # no yaxis ticks
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        # no xaxis ticks
+        ax.xaxis.set_ticks([])
+
+
+def cm2inch(*tupl):
+    inch = 2.54
+    if isinstance(tupl[0], tuple):
+        return tuple(i/inch for i in tupl[0])
+    else:
+        return tuple(i/inch for i in tupl)
+
+
+def custom_fig(name, size=(9, 8)):
+    return plt.figure(name, figsize=cm2inch(size))
+
 
 def load_csv(filename, labels):
     # create data dictionary
@@ -34,6 +73,7 @@ def load_csv(filename, labels):
 
     # return dictionary
     return data
+
 
 def load_csv_master(filename, labels = None):
     if labels is None:
--- exp2.py
+++ exp2.py
@@ -1,17 +1,40 @@
 from auxfuns import *
 from IPython import embed
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.stats import sem
 from time import sleep
+
+mpl.rcParams['axes.linewidth'] = 1.5
+
+# x axis
+mpl.rcParams['xtick.major.size'] = 4
+mpl.rcParams['xtick.major.width'] = 1.3
+mpl.rcParams['xtick.minor.size'] = 2
+mpl.rcParams['xtick.minor.width'] = 1.3
+
+# y axis
+mpl.rcParams['ytick.major.size'] = 4
+mpl.rcParams['ytick.major.width'] = 1.3
+mpl.rcParams['ytick.minor.size'] = 2
+mpl.rcParams['ytick.minor.width'] = 1.3
+
+
+mpl.rcParams['figure.subplot.bottom'] = 0.12
+mpl.rcParams['figure.subplot.right'] = 0.95
+
 
 # load relevant data
 data = load_csv_master('all_data01.txt')
 
 
+
 # calculate results
 
 # average for each fish
+sacc_amp = []
+slowphase_vel = []
 slowphase_gain = []
 tempfreqs = []
 spatfreqs = []
@@ -25,58 +48,155 @@ for fidx, fishid in enumerate(np.unique(data['fishid'])):
             spatfilt = data['spatfreq'] == sfreq
             filtvec = fishfilt & tempfilt & spatfilt
 
+            rsaccamp = np.abs(data['rsaccamp'][filtvec])
+            lsaccamp = np.abs(data['lsaccamp'][filtvec])
             rspslope = np.abs(data['rspslope'][filtvec])
             lspslope = np.abs(data['lspslope'][filtvec])
             stim_degpersec = tfreq / sfreq
 
             if len(rspslope) + len(lspslope) > 0:
-                slowphase_gain.append(np.nanmean(np.concatenate((rspslope, lspslope))) / stim_degpersec)
+                sacc_amp.append(np.nanmean(np.concatenate((rsaccamp, lsaccamp))))
+                slowphase_vel.append(np.nanmean(np.concatenate((rspslope, lspslope))))
+                slowphase_gain.append(slowphase_vel[-1] / stim_degpersec)
 
                 tempfreqs.append(tfreq)
                 spatfreqs.append(sfreq)
 # convert to array
+sacc_amp = np.asarray(sacc_amp)
+slowphase_vel = np.asarray(slowphase_vel)
 slowphase_gain = np.asarray(slowphase_gain)
 tempfreqs = np.asarray(tempfreqs)
 spatfreqs = np.asarray(spatfreqs)
 
 
 # average over fish
-gain_avg = []
-gain_sem = []
+sacc_amp_avg = []
+sacc_amp_sem = []
+slowphase_vel_avg = []
+slowphase_vel_sem = []
+slowphase_gain_avg = []
+slowphase_gain_sem = []
 avg_sfreqs = []
 avg_tfreqs = []
 for idx, (tfreq, sfreq)in enumerate(list(set(list(zip(tempfreqs, spatfreqs))))):  # too complicated to elaborate... works fine
     avg_sfreqs.append(sfreq)
     avg_tfreqs.append(tfreq)
 
+    amps = sacc_amp[(tempfreqs == tfreq) & (spatfreqs == sfreq)]
+    vels = slowphase_vel[(tempfreqs == tfreq) & (spatfreqs == sfreq)]
     gains = slowphase_gain[(tempfreqs == tfreq) & (spatfreqs == sfreq)]
 
-    gain_avg.append(np.nanmean(gains))
-    gain_sem.append(sem(gains, nan_policy='omit'))
-gain_avg = np.asarray(gain_avg)
-gain_sem = np.asarray(gain_sem)
+    sacc_amp_avg.append(np.nanmean(amps))
+    sacc_amp_sem.append(sem(amps, nan_policy='omit'))
+
+    slowphase_vel_avg.append(np.nanmean(vels))
+    slowphase_vel_sem.append(sem(vels, nan_policy='omit'))
+
+    slowphase_gain_avg.append(np.nanmean(gains))
+    slowphase_gain_sem.append(sem(gains, nan_policy='omit'))
+
 avg_sfreqs = np.asarray(avg_sfreqs)
 avg_tfreqs = np.asarray(avg_tfreqs)
 
+sacc_amp_avg = np.asarray(sacc_amp_avg)
+sacc_amp_sem = np.asarray(sacc_amp_sem)
 
+slowphase_vel_avg = np.asarray(slowphase_vel_avg)
+slowphase_vel_sem = np.asarray(slowphase_vel_sem)
+
+slowphase_gain_avg = np.asarray(slowphase_gain_avg)
+slowphase_gain_sem = np.asarray(slowphase_gain_sem)
+
+
+#############
 # plot data
 
+####
 # heatmap-like
-fig1 = plt.figure('Gain on Temporal / spatial')
-ax1 = fig1.add_subplot(1, 1, 1)
-gain_unique = np.unique(gain_avg)
-cmap = plt.get_cmap('viridis', lut=gain_unique.shape[0])
-for sfreq, tfreq, gain in zip(avg_sfreqs, avg_tfreqs, gain_avg):
-    ax1.loglog(sfreq, tfreq, marker='o', markersize=20, color=cmap.colors[gain_unique == gain][0])
+heatmap_size = (12.5, 11.5)
+xlim = [0.01, 0.3]
+ylim = [0.1, 5]
 
-ax1.set_xlabel('Spatial frequency [cyc/deg]')
-ax1.set_ylabel('Temporal frequency [cyc/s]')
 
+amp_unique = np.unique(sacc_amp_avg)
+vel_unique = np.unique(slowphase_vel_avg)
+gain_unique = np.unique(slowphase_gain_avg)
+
+fig11 = custom_fig('Saccade amplitude on Temporal / spatial', heatmap_size)
+ax11 = fig11.add_subplot(1, 1, 1)
+# add colorbar
+minval = np.min(np.floor(amp_unique))
+maxval = np.ceil(np.max(amp_unique))
+zticks = np.arange(minval, maxval + 0.01)
+cax = ax11.imshow(np.concatenate((zticks, zticks)).reshape((2, -1)), interpolation='nearest', cmap='viridis')
+cbar = fig11.colorbar(cax, ticks=zticks)
+cbar.ax.set_ylabel('Saccade amplitude [deg]')
+sacc_amp_cmap = np.asarray(cbar.cmap.colors)
+sacc_amp_valmap = np.arange(minval, maxval, (maxval - minval) / sacc_amp_cmap.shape[0])
+plt.cla()  # clears imshow plot, but keeps the colorbar
+
+
+fig12 = custom_fig('Slowphase velocity on Temporal / spatial', heatmap_size)
+ax12 = fig12.add_subplot(1, 1, 1)
+# add colorbar
+minval = np.min(np.floor(vel_unique))
+maxval = np.ceil(np.max(vel_unique))
+zticks = np.arange(minval, maxval + 0.01)
+cax = ax12.imshow(np.concatenate((zticks, zticks)).reshape((2, -1)), interpolation='nearest', cmap='viridis')
+cbar = fig12.colorbar(cax, ticks=zticks)
+cbar.ax.set_ylabel('Slowphase velocity [deg/s]')
+slowphase_vel_cmap = np.asarray(cbar.cmap.colors)
+slowphase_vel_valmap = np.arange(minval, maxval, (maxval - minval) / slowphase_vel_cmap.shape[0])
+plt.cla()  # clears imshow plot, but keeps the colorbar
+
+fig13 = custom_fig('Slowphase gain on Temporal / spatial', heatmap_size)
+ax13 = fig13.add_subplot(1, 1, 1)
+# add colorbar
+minval = np.floor(np.min(gain_unique) * 10) / 10
+maxval = np.ceil(np.max(gain_unique) * 10) / 10
+zticks = np.arange(minval, maxval + 0.01, 0.1)
+cax = ax13.imshow(np.concatenate((zticks, zticks)).reshape((2, -1)), interpolation='nearest', cmap='viridis')
+cbar = fig13.colorbar(cax, ticks=zticks)
+cbar.ax.set_ylabel('Slowphase gain')
+slowphase_gain_cmap = np.asarray(cbar.cmap.colors)
+slowphase_gain_valmap = np.arange(minval, maxval, (maxval - minval) / slowphase_gain_cmap.shape[0])
+plt.cla()  # clears imshow plot, but keeps the colorbar
+
+
+for sfreq, tfreq, amp, vel, gain in zip(avg_sfreqs, avg_tfreqs, sacc_amp_avg, slowphase_vel_avg, slowphase_gain_avg):
+    ax11.loglog(sfreq, tfreq, marker='o', markersize=20, color=sacc_amp_cmap[np.argmin(np.abs(sacc_amp_valmap - amp)), :])
+    ax12.loglog(sfreq, tfreq, marker='o', markersize=20, color=slowphase_vel_cmap[np.argmin(np.abs(slowphase_vel_valmap - vel)), :])
+    ax13.loglog(sfreq, tfreq, marker='o', markersize=20, color=slowphase_gain_cmap[np.argmin(np.abs(slowphase_gain_valmap - gain)), :])
+
+ax11.set_xlabel('Spatial frequency [cyc/deg]')
+ax11.set_ylabel('Temporal frequency [cyc/s]')
+ax11.set_xlim(xlim)
+ax11.set_ylim(ylim)
+adjust_spines(ax11)
+fig11.savefig('../sacc_amp_map.eps', format='eps')
+
+ax12.set_xlabel('Spatial frequency [cyc/deg]')
+ax12.set_ylabel('Temporal frequency [cyc/s]')
+ax12.set_xlim(xlim)
+ax12.set_ylim(ylim)
+adjust_spines(ax12)
+fig12.savefig('../slowphase_vel_map.eps', format='eps')
+
+ax13.set_xlabel('Spatial frequency [cyc/deg]')
+ax13.set_ylabel('Temporal frequency [cyc/s]')
+ax13.set_xlim(xlim)
+ax13.set_ylim(ylim)
+adjust_spines(ax13)
+fig13.savefig('../slowphase_gain_map.eps', format='eps')
+
+
+plt.show()
+exit()
 # function of spatial freq
 fig2 = plt.figure('Gain / spatial (tf=0.8)')
 # sort data
 const_tf = np.round(avg_tfreqs, 1) == 0.8
-sfs, gains, sems = list(zip(*sorted(zip(avg_sfreqs[const_tf], gain_avg[const_tf], gain_sem[const_tf]))))
+sfs, gains, sems = list(zip(*sorted(zip(avg_sfreqs[const_tf], slowphase_gain_avg[const_tf], slowphase_gain_sem[const_tf]))))
 
 ax2 = fig2.add_subplot(1, 1, 1)
 ax2.semilogx()
@@ -89,7 +209,7 @@ ax2.set_ylabel('Gain')
 fig2 = plt.figure('Gain / temporal (sf=0.06)')
 # sort data
 const_sf = np.round(avg_sfreqs, 2) == 0.06
-tfs, gains, sems = list(zip(*sorted(zip(avg_tfreqs[const_sf], gain_avg[const_sf], gain_sem[const_sf]))))
+tfs, gains, sems = list(zip(*sorted(zip(avg_tfreqs[const_sf], slowphase_gain_avg[const_sf], slowphase_gain_sem[const_sf]))))
 
 ax2 = fig2.add_subplot(1, 1, 1)
 ax2.semilogx()
@@ -102,7 +222,7 @@ ax2.set_ylabel('Gain')
 fig3 = plt.figure('(const velocity) Gain / spatial')
 # sort data
 const_v = np.round(avg_sfreqs / avg_tfreqs, 2) == 0.08
-sfs, gains, sems = list(zip(*sorted(zip(avg_sfreqs[const_v], gain_avg[const_v], gain_sem[const_v]))))
+sfs, gains, sems = list(zip(*sorted(zip(avg_sfreqs[const_v], slowphase_gain_avg[const_v], slowphase_gain_sem[const_v]))))
 
 ax3 = fig3.add_subplot(1, 1, 1)
 ax3.semilogx()
@@ -115,7 +235,7 @@ ax3.set_ylabel('Gain')
 fig3 = plt.figure('(const velocity) Gain / temporal')
 # sort data
 const_v = np.round(avg_sfreqs / avg_tfreqs, 2) == 0.08
-tfs, gains, sems = list(zip(*sorted(zip(avg_tfreqs[const_v], gain_avg[const_v], gain_sem[const_v]))))
+tfs, gains, sems = list(zip(*sorted(zip(avg_tfreqs[const_v], slowphase_gain_avg[const_v], slowphase_gain_sem[const_v]))))
 
 ax3 = fig3.add_subplot(1, 1, 1)
 ax3.semilogx()
